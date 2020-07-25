bc6cde46fea63f27effa0789e9be27c0def3fd36
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-07-26 00:26:07 +0530

    task 2 done

---
+++ lib/draw.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Draw extends StatefulWidget {
+  @override
+  _DrawState createState() => _DrawState();
+}
+
+class _DrawState extends State<Draw> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:ApptitudeApp/draw.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -31,17 +32,7 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     ),
-    Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Page3',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-    ),
+    Draw(),
     Scaffold(
       backgroundColor: Colors.black,
       body: Center(
@@ -86,238 +77,246 @@ class _HomePageState extends State<HomePage> {
               0xff000000), //or any other color you want. e.g Colors.blue.withOpacity(0.5)
         ),
         child: Drawer(
-            child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: 16, top: 40, right: 16),
-              width: double.infinity,
-              height: 200,
-              child: Container(
-                alignment: Alignment.center,
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 16, top: 40, right: 16),
                 width: double.infinity,
-                child: Text(
-                  'Carry Vai',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xffe63946),
-                  ),
-                ),
-              ),
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: Color(0xffe63946)),
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-                color: Color(0xff00000),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton(
-              child: Container(
-                margin: EdgeInsets.only(left: 2, right: 2),
-                width: double.infinity,
-                height: 70,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Color(0xffe63946)),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color:
-                      _currentIndex == 0 ? Color(0xffe63946) : Color(0xff00000),
-                ),
-                child: Center(
+                height: 200,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
                   child: Text(
-                    'About Us',
+                    'Carry Vai',
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 30,
                       fontWeight: FontWeight.w600,
-                      color: _currentIndex == 0
-                          ? Color(0xff000000)
-                          : Color(0xffe63946),
+                      color: Color(0xffe63946),
                     ),
                   ),
                 ),
-              ),
-              onPressed: () {
-                setState(() {
-                  _currentIndex = 0;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton(
-              child: Container(
-                margin: EdgeInsets.only(left: 2, right: 2),
-                width: double.infinity,
-                height: 70,
                 decoration: BoxDecoration(
                   border: Border.all(width: 2, color: Color(0xffe63946)),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color:
-                      _currentIndex == 1 ? Color(0xffe63946) : Color(0xff00000),
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  color: Color(0xff00000),
                 ),
-                child: Center(
-                  child: Text(
-                    'Image Fetcher',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: _currentIndex == 1
-                          ? Color(0xff000000)
-                          : Color(0xffe63946),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                child: Container(
+                  margin: EdgeInsets.only(left: 2, right: 2),
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xffe63946)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: _currentIndex == 0
+                        ? Color(0xffe63946)
+                        : Color(0xff00000),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'About Us',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: _currentIndex == 0
+                            ? Color(0xff000000)
+                            : Color(0xffe63946),
+                      ),
                     ),
                   ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 0;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              onPressed: () {
-                setState(() {
-                  _currentIndex = 1;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton(
-              child: Container(
-                margin: EdgeInsets.only(left: 2, right: 2),
-                width: double.infinity,
-                height: 70,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Color(0xffe63946)),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color:
-                      _currentIndex == 2 ? Color(0xffe63946) : Color(0xff00000),
-                ),
-                child: Center(
-                  child: Text(
-                    'Draw',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: _currentIndex == 2
-                          ? Color(0xff000000)
-                          : Color(0xffe63946),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                child: Container(
+                  margin: EdgeInsets.only(left: 2, right: 2),
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xffe63946)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: _currentIndex == 1
+                        ? Color(0xffe63946)
+                        : Color(0xff00000),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Image Fetcher',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: _currentIndex == 1
+                            ? Color(0xff000000)
+                            : Color(0xffe63946),
+                      ),
                     ),
                   ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 1;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              onPressed: () {
-                setState(() {
-                  _currentIndex = 2;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton(
-              child: Container(
-                margin: EdgeInsets.only(left: 2, right: 2),
-                width: double.infinity,
-                height: 70,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Color(0xffe63946)),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color:
-                      _currentIndex == 3 ? Color(0xffe63946) : Color(0xff00000),
-                ),
-                child: Center(
-                  child: Text(
-                    'Fourth',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: _currentIndex == 3
-                          ? Color(0xff000000)
-                          : Color(0xffe63946),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                child: Container(
+                  margin: EdgeInsets.only(left: 2, right: 2),
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xffe63946)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: _currentIndex == 2
+                        ? Color(0xffe63946)
+                        : Color(0xff00000),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Draw',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: _currentIndex == 2
+                            ? Color(0xff000000)
+                            : Color(0xffe63946),
+                      ),
                     ),
                   ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 2;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              onPressed: () {
-                setState(() {
-                  _currentIndex = 3;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton(
-              child: Container(
-                margin: EdgeInsets.only(left: 2, right: 2),
-                width: double.infinity,
-                height: 70,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Color(0xffe63946)),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color:
-                      _currentIndex == 4 ? Color(0xffe63946) : Color(0xff00000),
-                ),
-                child: Center(
-                  child: Text(
-                    'Fifth',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: _currentIndex == 4
-                          ? Color(0xff000000)
-                          : Color(0xffe63946),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                child: Container(
+                  margin: EdgeInsets.only(left: 2, right: 2),
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xffe63946)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: _currentIndex == 3
+                        ? Color(0xffe63946)
+                        : Color(0xff00000),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Fourth',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: _currentIndex == 3
+                            ? Color(0xff000000)
+                            : Color(0xffe63946),
+                      ),
                     ),
                   ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 3;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              onPressed: () {
-                setState(() {
-                  _currentIndex = 4;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            FlatButton(
-              child: Container(
-                margin: EdgeInsets.only(left: 2, right: 2),
-                width: double.infinity,
-                height: 70,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Color(0xffe63946)),
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color:
-                      _currentIndex == 5 ? Color(0xffe63946) : Color(0xff00000),
-                ),
-                child: Center(
-                  child: Text(
-                    'Sixth',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: _currentIndex == 5
-                          ? Color(0xff000000)
-                          : Color(0xffe63946),
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                child: Container(
+                  margin: EdgeInsets.only(left: 2, right: 2),
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xffe63946)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: _currentIndex == 4
+                        ? Color(0xffe63946)
+                        : Color(0xff00000),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Fifth',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: _currentIndex == 4
+                            ? Color(0xff000000)
+                            : Color(0xffe63946),
+                      ),
                     ),
                   ),
                 ),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 4;
+                  });
+                  Navigator.pop(context);
+                },
               ),
-              onPressed: () {
-                setState(() {
-                  _currentIndex = 5;
-                });
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(
-              height: 10,
-            ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              FlatButton(
+                child: Container(
+                  margin: EdgeInsets.only(left: 2, right: 2),
+                  width: double.infinity,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xffe63946)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: _currentIndex == 5
+                        ? Color(0xffe63946)
+                        : Color(0xff00000),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Sixth',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: _currentIndex == 5
+                            ? Color(0xff000000)
+                            : Color(0xffe63946),
+                      ),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 5;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         )
             /*ListView(
             children: [
